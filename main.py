c93504ad020c608ff299f38c497ab7a06b8b8c15
VitorMoreira137 <vitor.moreira@wabtec.com>
2025-02-10 17:11:38 -0300

    2nd commit ever

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 string1 = ("I am Optimus Prime, and I send this message to any surviving Autobots taking refuge among the stars: "
-           "We are here. We are waiting.")git as
+           "We are here. We are waiting.")
+string2 = "The Truth? The Truth is that I am god, boy. From a land far from here."
 print(string1)
